d6a04a4362090a9e45cf3fcbe7518a9997f1a49e
Jun Kawasaki <jun784@gmail.com>
2026-08-03 15:59:53 +0900

    Adopt inferred option fallback syntax

--- test/annotation/core_test.clj
+++ test/annotation/core_test.clj
@@ -3,7 +3,7 @@
             [clojure.java.shell :as shell]
             [clojure.test :refer [deftest is]]
             [kotoba.compiler.core :as compiler]
-            [kotoba.compiler.ir :as ir]))
+            [kotoba.kir :as ir]))
 
 (def source (slurp "src/annotation/core.kotoba"))
 
@@ -18,13 +18,14 @@
 (deftest source-is-a-real-kotoba-document-library
   (let [checked (compiler/check-source source)
         kir (ir/lower (:hir checked))
-        props ["map" [[:id ["string" "urn:annotation:1"]]
-                       [:target ["string" "https://example.test/doc"]]]]
+        props ["map" [[["keyword" :id] ["string" "urn:annotation:1"]]
+                       [["keyword" :target] ["string" "https://example.test/doc"]]]]
         annotation (ir/execute kir 'annotation [props])
         validation (ir/execute kir 'validate [annotation])]
     (is (= :kotoba.hir/v3 (get-in checked [:hir :format])))
     (is (= "map" (first annotation)))
-    (is (= ["map" [[:errors ["vector" []]] [:valid? ["bool" true]]]] validation))))
+    (is (= ["map" [[["keyword" :errors] ["vector" []]]
+                    [["keyword" :valid?] ["bool" true]]]] validation))))
 
 (deftest compiles-to-restricted-javascript-and-real-wasm
   (let [javascript (compiler/compile-source source :js-kotoba-v1)
@@ -32,12 +33,12 @@
         js-probe
         (shell/sh "node" "--input-type=module" "-e"
                   (str "import('data:text/javascript;base64," (base64 (.getBytes ^String (:source javascript) "UTF-8"))
-                       "').then(m=>{const x=m.instantiateKotoba({}),p=['map',[[\":target\",['string','urn:test']]]],a=x.annotation(p);"
+                       "').then(m=>{const x=m.instantiateKotoba({}),p=['map',[[['keyword',':target'],['string','urn:test']]]],a=x.annotation(p);"
                        "const v=x.validate(a);if(a[0]!=='map'||v[1][1][1][1]!==true)process.exit(2)})"))
         wasm-probe
         (shell/sh "node" "--input-type=module" "-e"
                   (str "import(process.argv[1]).then(async m=>{const h=await m.instantiateKotoba(Buffer.from(process.argv[2],'base64'));"
-                       "const x=h.instance.exports,p=h.typedValues.document(['map',[[\":target\",['string','urn:test']]]]),a=x.annotation(p);"
+                       "const x=h.instance.exports,p=h.typedValues.document(['map',[[['keyword',':target'],['string','urn:test']]]]),a=x.annotation(p);"
                        "if(a[0]!=='map'||x.validate(a)[1][1][1][1]!==true)process.exit(2)})"
                        ".catch(e=>{console.error(e);process.exit(70)})")
                   (.toString (.toUri (.resolve (compiler-root) "runtime/browser-host.mjs")))
